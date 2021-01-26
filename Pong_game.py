47f6a77040f6a80de5c552fa26735ad1ce4a23e8
raghu <raghumina11@gmail.com>
2021-01-26 20:57:36 +0530

    Create Pong_game.py

---
+++ Pong_game.py
@@ -0,0 +1,19 @@
+# Pong game
+# Classic pong game
+# Made by using Turtle module in python
+
+# Lets Start
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Raghu")
+wn.bgcolor("blue")
+wn.setup(width=800,height=800)
+wn.tracer(0)
+
+
+# Main game loop
+#
+while True:
+    wn.update()
+
