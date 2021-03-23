f7455ec5c9b3f02ca0972c73f7d0eac0727b0a2f
raghu <raghumina11@gmail.com>
2021-03-23 23:14:48 +0530

    Update Pong_game.py

--- Pong_game.py
+++ Pong_game.py
@@ -1,289 +1,230 @@
-import pygame, sys, random
-
-# -------
-# CLASSES
-# -------
-
-class Paddle:
-	def __init__(self, screen, color, posX, posY, width, height):
-		self.screen = screen
-		self.color = color
-		self.posX = posX
-		self.posY = posY
-		self.width = width
-		self.height = height
-		self.state = 'stopped'
-		self.draw()
-
-	def draw(self):
-		pygame.draw.rect( self.screen, self.color, (self.posX, self.posY, self.width, self.height) )
-
-	def move(self):
-		# moving up
-		if self.state == 'up':
-			self.posY -= 10
-
-		# moving down
-		elif self.state == 'down':
-			self.posY += 10
-
-	def clamp(self):
-		if self.posY <= 0:
-			self.posY = 0
-
-		if self.posY + self.height >= HEIGHT:
-			self.posY = HEIGHT - self.height
-
-	def restart_pos(self):
-		self.posY = HEIGHT//2 - self.height//2
-		self.state = 'stopped'
-		self.draw()
-
-class Ball:
-	def __init__(self, screen, color, posX, posY, radius):
-		self.screen = screen
-		self.color = color
-		self.posX = posX
-		self.posY = posY
-		self.dx = 0
-		self.dy = 0
-		self.radius = radius
-		self.draw()
-
-	def draw(self):
-		pygame.draw.circle( self.screen, self.color, (self.posX, self.posY), self.radius )
-
-	def start(self):
-		# this will be random
-		self.dx = 15
-		self.dy = 5
-
-	def move(self):
-		self.posX += self.dx
-		self.posY += self.dy
-
-	def wall_collision(self):
-		self.dy = -self.dy
-
-	def paddle_collision(self):
-		self.dx = -self.dx
-
-	def restart_pos(self):
-		self.posX = WIDTH//2
-		self.posY = HEIGHT//2
-		self.dx = 0
-		self.dy = 0
-		self.draw()
-
-class PlayerScore:
-	def __init__(self, screen, points, posX, posY):
-		self.screen = screen
-		self.points = points
-		self.posX = posX
-		self.posY = posY
-		self.font = pygame.font.SysFont("monospace", 80, bold=True)
-		self.label = self.font.render(self.points, 0, WHITE)
-		self.show()
-
-	def show(self):
-		self.screen.blit(self.label, (self.posX - self.label.get_rect().width // 2, self.posY))
-
-	def increase(self):
-		points = int(self.points) + 1
-		self.points = str(points)
-		self.label = self.font.render(self.points, 0, WHITE)
-
-	def restart(self):
-		self.points = '0'
-		self.label = self.font.render(self.points, 0, WHITE)
-
-class CollisionManager:
-	def between_ball_and_paddle1(self, ball, paddle):
-		ballX = ball.posX
-		ballY = ball.posY
-		paddleX = paddle.posX
-		paddleY = paddle.posY
-
-		# y is in collision area?
-		if ballY + ball.radius > paddleY and ballY - ball.radius < paddleY + paddle.height:
-			# x is in collision area?
-			if ballX - ball.radius <= paddleX + paddle.width:
-				# collision
-				return True
-
-		# no collision
-		return False
-
-	def between_ball_and_paddle2(self, ball, paddle):
-		ballX = ball.posX
-		ballY = ball.posY
-		paddleX = paddle.posX
-		paddleY = paddle.posY
-
-		# y is in collision?
-		if ballY + ball.radius > paddleY and ballY - ball.radius < paddleY + paddle.height:
-			# x is in collision?
-			if ballX + ball.radius >= paddleX:
-				# collision
-				return True
-
-		# no collision
-		return False
-
-	def between_ball_and_walls(self, ball):
-		ballY = ball.posY
-
-		# top collision
-		if ballY - ball.radius <= 0:
-			return True
-
-		# bottom collision
-		if ballY + ball.radius >= HEIGHT:
-			return True
-
-		# no collision
-		return False
-
-	def between_ball_and_goal1(self, ball):
-		return ball.posX + ball.radius <= 0
-
-	def between_ball_and_goal2(self, ball):
-		return ball.posX - ball.radius >= WIDTH
-
-# ---------------------------------------------
-
-# ---------
-# CONSTANTS
-# ---------
-WIDTH, HEIGHT = 900, 500
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-
-# SCREEN
-pygame.init()
-screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
-pygame.display.set_caption('PONG')
-
-# ---------
-# FUNCTIONS
-# ---------
-def draw_board():
-	screen.fill( BLACK )
-	pygame.draw.line( screen, WHITE, (WIDTH//2, 0), (WIDTH//2, HEIGHT), 5 )
-
-def restart():
-	draw_board()
-	score1.restart()
-	score2.restart()
-	ball.restart_pos()
-	paddle1.restart_pos()
-	paddle2.restart_pos()
-
-draw_board()
-
-# -------
-# OBJECTS
-# -------
-paddle1 = Paddle( screen, WHITE, 15, HEIGHT//2 - 60, 20, 120 )
-paddle2 = Paddle( screen, WHITE, WIDTH - 20 - 15, HEIGHT//2 - 60, 20, 120 )
-ball = Ball( screen, WHITE, WIDTH//2, HEIGHT//2, 12 )
-collision = CollisionManager()
-score1 = PlayerScore( screen, '0', WIDTH//4, 15 )
-score2 = PlayerScore( screen, '0', WIDTH - WIDTH//4, 15 )
-
-# ---------
-# VARIABLES
-# ---------
-playing = False
-
-# --------
-# MAINLOOP
-# --------
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_p and not playing:
-				ball.start()
-				playing = True
-
-			if event.key == pygame.K_r and playing:
-				restart()
-				playing = False
-
-			if event.key == pygame.K_w:
-				paddle1.state = 'up'
-
-			if event.key == pygame.K_s:
-				paddle1.state = 'down'
-
-			if event.key == pygame.K_UP:
-				paddle2.state = 'up'
-
-			if event.key == pygame.K_DOWN:
-				paddle2.state = 'down'
-
-		if event.type == pygame.KEYUP:
-			paddle1.state = 'stopped'
-			paddle2.state = 'stopped'
-
-	if playing:
-		draw_board()
-
-		# ball
-		ball.move()
-		ball.draw()
-
-		# paddle 1
-		paddle1.move()
-		paddle1.clamp()
-		paddle1.draw()
-
-		# paddle 2
-		paddle2.move()
-		paddle2.clamp()
-		paddle2.draw()
-
-		# wall collision
-		if collision.between_ball_and_walls(ball):
-			print('WALL COLLISION')
-			ball.wall_collision()
-
-		# paddle1 collision
-		if collision.between_ball_and_paddle1(ball, paddle1):
-			print('COLLISION WITH PADDLE 1')
-			ball.paddle_collision()
-
-		# paddle2 collision
-		if collision.between_ball_and_paddle2(ball, paddle2):
-			print('COLLISION WITH PADDLE 2')
-			ball.paddle_collision()
-
-		# GOAL OF PLAYER 1 !
-		if collision.between_ball_and_goal2(ball):
-			draw_board()
-			score1.increase()
-			ball.restart_pos()
-			paddle1.restart_pos()
-			paddle2.restart_pos()
-			playing = False
-
-		# GOAL OF PLAYER 2!
-		if collision.between_ball_and_goal1(ball):
-			draw_board()
-			score2.increase()
-			ball.restart_pos()
-			paddle1.restart_pos()
-			paddle2.restart_pos()
-			playing = False
-
-	score1.show()
-	score2.show()
-
-	pygame.display.update()
+# GUI.py
+# Sudoku in gui
 
 
+import pygame
+from solver import solve, valid
+import time
+pygame.font.init()
 
+
+class Grid:
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+    def __init__(self, rows, cols, width, height):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.width = width
+        self.height = height
+        self.model = None
+        self.selected = None
+
+    def update_model(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
+    def place(self, val):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set(val)
+            self.update_model()
+
+            if valid(self.model, val, (row,col)) and solve(self.model):
+                return True
+            else:
+                self.cubes[row][col].set(0)
+                self.cubes[row][col].set_temp(0)
+                self.update_model()
+                return False
+
+    def sketch(self, val):
+        row, col = self.selected
+        self.cubes[row][col].set_temp(val)
+
+    def draw(self, win):
+        # Draw Grid Lines
+        gap = self.width / 9
+        for i in range(self.rows+1):
+            if i % 3 == 0 and i != 0:
+                thick = 4
+            else:
+                thick = 1
+            pygame.draw.line(win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
+            pygame.draw.line(win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
+
+        # Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(win)
+
+    def select(self, row, col):
+        # Reset all other
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].selected = False
+
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
+
+    def clear(self):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set_temp(0)
+
+    def click(self, pos):
+        """
+        :param: pos
+        :return: (row, col)
+        """
+        if pos[0] < self.width and pos[1] < self.height:
+            gap = self.width / 9
+            x = pos[0] // gap
+            y = pos[1] // gap
+            return (int(y),int(x))
+        else:
+            return None
+
+    def is_finished(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cubes[i][j].value == 0:
+                    return False
+        return True
+
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__(self, value, row, col, width ,height):
+        self.value = value
+        self.temp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+
+    def draw(self, win):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.temp != 0 and self.value == 0:
+            text = fnt.render(str(self.temp), 1, (128,128,128))
+            win.blit(text, (x+5, y+5))
+        elif not(self.value == 0):
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+
+        if self.selected:
+            pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
+
+    def set(self, val):
+        self.value = val
+
+    def set_temp(self, val):
+        self.temp = val
+
+
+def redraw_window(win, board, time, strikes):
+    win.fill((255,255,255))
+    # Draw time
+    fnt = pygame.font.SysFont("comicsans", 40)
+    text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
+    win.blit(text, (540 - 160, 560))
+    # Draw Strikes
+    text = fnt.render("X " * strikes, 1, (255, 0, 0))
+    win.blit(text, (20, 560))
+    # Draw grid and board
+    board.draw(win)
+
+
+def format_time(secs):
+    sec = secs%60
+    minute = secs//60
+    hour = minute//60
+
+    mat = " " + str(minute) + ":" + str(sec)
+    return mat
+
+
+def main():
+    win = pygame.display.set_mode((540,600))
+    pygame.display.set_caption("Sudoku")
+    board = Grid(9, 9, 540, 540)
+    key = None
+    run = True
+    start = time.time()
+    strikes = 0
+    while run:
+
+        play_time = round(time.time() - start)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    key = 1
+                if event.key == pygame.K_2:
+                    key = 2
+                if event.key == pygame.K_3:
+                    key = 3
+                if event.key == pygame.K_4:
+                    key = 4
+                if event.key == pygame.K_5:
+                    key = 5
+                if event.key == pygame.K_6:
+                    key = 6
+                if event.key == pygame.K_7:
+                    key = 7
+                if event.key == pygame.K_8:
+                    key = 8
+                if event.key == pygame.K_9:
+                    key = 9
+                if event.key == pygame.K_DELETE:
+                    board.clear()
+                    key = None
+                if event.key == pygame.K_RETURN:
+                    i, j = board.selected
+                    if board.cubes[i][j].temp != 0:
+                        if board.place(board.cubes[i][j].temp):
+                            print("Success")
+                        else:
+                            print("Wrong")
+                            strikes += 1
+                        key = None
+
+                        if board.is_finished():
+                            print("Game over")
+                            run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                clicked = board.click(pos)
+                if clicked:
+                    board.select(clicked[0], clicked[1])
+                    key = None
+
+        if board.selected and key != None:
+            board.sketch(key)
+
+        redraw_window(win, board, play_time, strikes)
+        pygame.display.update()
+
+
+main()
+pygame.quit()
